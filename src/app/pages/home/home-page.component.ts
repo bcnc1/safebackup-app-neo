994d7eff5d345ed9eb7d405dd2c638201c1c036a
kimcy <kim.cy@outlook.com>
2019-07-24 10:26:55 +0900

    화면에 폴더/파일에 따라 내용을 표시하려고 만든건데 죽어서 막음

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -184,7 +184,7 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
     this.logger.debug('SUBFOLDER', gotoPath, this.parentFolder);
     //kimcy: 이걸보여주면? 무엇이?
-    this.showingFolderList.push(this.parentFolder); //test code
+    //this.showingFolderList.push(this.parentFolder); //test code
 
     //kimcy: 추후 새로..
     // this.postAPI.list(this.board.id, {
